91d28a18629a284e135fd57928cff51f028f9c0c
Joko Susilo <susilo.j8@gmail.com>
2018-05-16 17:59:54 +0700

    fetching data posts in timeline from graphql

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,45 +1,61 @@
 import React, { Component } from 'react';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import "./Post.css";
 
-class Post extends Component {
+const Post = () => {
+    return (
+        <Query
+            query={gql`
+                {
+                    post(user_id: "a", post_id: "a") {
+                        image
+                        caption
+                        user {
+                            nickname
+                            avatar
+                        }
+                    }
+                }
+            `}
+        >
+            {({ loading, error, data }) => {
+                if(loading) return <p>Loading Post ...</p>;
+                if(error) return <p>Error Loading Post :( </p>;
+                let image = data.post.image;
+                let caption = data.post.caption;
+                let user = data.post.user;
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        const nickname = this.props.nickname;
-        const avatar = this.props.avatar;
-        const image = this.props.image;
-        const caption = this.props.caption;
-
-        return (
-            <article className="Post" ref="Post">
-                <header>
-                    <div className="Post-user">
-                        <div className="Post-user-avatar">
-                            <img src={avatar} alt={nickname} />
+                return (
+                    <article className="Post" ref="Post">
+                        <header>
+                            <div className="Post-user">
+                                <div className="Post-user-avatar">
+                                    <img src={user.avatar} alt={user.nickname} />
+                                </div>
+                                <div className="Post-user-nickname">
+                                    <span>{user.nickname}</span>
+                                </div>
+                            </div>
+                        </header>
+                        <div className="Post-image">
+                            <div className="Post-image-bg">
+                                <img src={image} alt={caption} />
+                            </div>
                         </div>
-                        <div className="Post-user-nickname">
-                            <span>{nickname}</span>
+                        <div className="Post-user">
+                            <div className="Post-user-nickname">
+                                <span>{user.nickname}</span>
+                            </div>
+                            <div className="Post-caption">
+                                <span>{caption}</span>
+                            </div>
                         </div>
-                    </div>
-                </header>
-                <div className="Post-image">
-                    <div className="Post-image-bg">
-                        <img src={image} alt={caption} />
-                    </div>
-                </div>
-                <div className="Post-caption">
-                    <div className="Post-user-nickname">
-                        <strong>{nickname}</strong>
-                    </div>
-                    {caption}
-                </div>
-            </article>
-        );
-    }
-}
+                    </article>
+                );
+            }}
+        </Query>
+    );
+};
 
 export default Post;
